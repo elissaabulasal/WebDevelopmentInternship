45f6416806171c857dad096946a8e5f20996eab7
Ahmad Alhaich <ama240@student.bau.edu.lb>
2023-05-26 18:41:09 +0300

    updated buying section

---
+++ lib/routes/routes.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+
+import '../components/buying_section/all_items.dart';
+import '../components/buying_section/item_by_id.dart';
+import '../screens/home_screen.dart';
+import '../screens/start_sceen.dart';
+
+class AppRoutes {
+  static const String startScreen = '/';
+  static const String homeScreen = '/home';
+  static const String buyingSection = '/buying_section';
+  static const String itemDetails = '/item_details';
+
+  static final routes = [
+    GetPage(name: startScreen, page: () => StartScreen()),
+    GetPage(name: homeScreen, page: () => HomeScreen()),
+    GetPage(name: buyingSection, page: () => BuyingSection()),
+    GetPage(
+        name: itemDetails,
+        page: () => const ItemDetails(
+              description: '',
+              itemName: '',
+              price: 0,
+            )),
+  ];
+}
